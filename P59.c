03df82b21626c7bc0fb6798647c64ca3c717c664
krisha-12569 <kdmorbi36@gmail.com>
2025-07-11 19:03:44 +0530

    Updated remove duplicate

--- P59.c
+++ P59.c
@@ -1,95 +1,82 @@
 #include<stdio.h>
 #include<stdlib.h>
-void insertElement(int x);
-void compare();
-void display();
 
 struct node{
     int info;
     struct node *link;
 };
 struct node *first=NULL;
-
-void insertElement(int x){
-    struct node *newNode=(struct node *)malloc(sizeof(struct node));
-    struct node *save;
-    newNode->info=x;
-    newNode->link=NULL;
-
-    if(first=NULL){
-        first= newNode;
-    }
-    else{
-        save=first;
-        while(save->link!=NULL){
-            save=save->link;
-        }
-        save->link=newNode;
-    }
-
-}
-
-
-void compare(){
-    struct node *temp=first;
-
-    if(first=NULL){
-        printf("Empty\n");
+void insertelement(struct node *first){
+     struct node *newNode = (struct node *)malloc(sizeof(struct node));
+    int data;
+    printf("Enter Value : ");
+    scanf("%d", &data);
+    newNode->info = data;
+    newNode->link = NULL;
+    struct node *save = first;
+    if (first == NULL)
+    {
+        first = newNode;
         return;
     }
-    else{
-        while(temp->link!=NULL && temp!=NULL){
-            if(temp->info==temp->link->info){
-                temp->link=temp->link->link;
-            }
-            else{
-                temp=temp->link;
-            }
-        }
+    while (save->link != NULL)
+    {
+        save = save->link;
     }
+    save->link = newNode;
+    
 }
+void removeduplicate(struct node *first){
+  struct node *current=first;
 
-
-void display(){
-    struct node *current=first;
-
-    if(first==NULL){
-        printf("Empty");
-        return;
+  while(current!=NULL && current->link!=NULL){
+    if(current->info== current->link->info){
+        struct node *dup=current->link;
+        current->link=current->link->link;
+        free(dup);
     }
-    while(current!=NULL){
-        printf("|%d|-->",current->info);
+    else{
         current=current->link;
     }
+  }
+
+}
+void display()
+{
+    struct node *temp = first;
+    if (first == NULL)
+    {
+        printf("Empty List");
+        return;
+    }
+    while (temp != NULL)
+    {
+        printf("%d -> ", temp->info);
+        temp = temp->link;
+    }
+    printf("\n");
 }
 
 int main(){
-    int a,choice;
+    int choice;
     while(1){
-    printf("Enter 1 for Insertion\n Enter 2 to compare\nEnter 3 to display list\nEnter 4 to exit:");
-    printf("Enter choice:");
-    scanf("%d",&choice);
+        printf("1 To Insert elements\n2 remove duplicate\n3 To Display Elements\n4 To Exit\n ");
+        scanf("%d", &choice);
 
-    switch(choice){
-        case 1:
-          printf("Enter Element:");
-          scanf("%d",&a);
-
-          insertElement(a);
-          break;
-    
-      case 2:
-        compare();
-        break;
-    
-      case 3:
-      display();
-      break;
-      case 4:
-            printf("---Exiting---");
-            return 0;
+        switch(choice){
+            case 1:
+            insertelement(first);
+            break;
+            case 2:
+            removeduplicate(first);
+            break;
+            case 3:
+             display();
+              break;
+            case 4:
+             printf("---Exiting---");
+             return 0;
         }
-    }
-     return 0;
+     }
 }
 
